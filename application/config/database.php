5b6d5886a9921889f573fecc9543610493453863
artadian <artadianw@gmail.com>
2021-01-25 14:54:19 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -73,28 +73,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-// $db['default'] = array(
-// 	'dsn'	   => 'mysql:host=db6.wismilak.com;dbname=dev_mimos',
-
-// 	'hostname' => "db6.wismilak.com",
-// 	'username' => "dev",
-// 	'password' => "wisdev989",
-// 	'database' => "dev_mimos",
-// 	'dbdriver' => 'pdo',
-// 	'dbprefix' => '',
-// 	'pconnect' => FALSE,
-// 	'db_debug' => (ENVIRONMENT !== 'production'),
-// 	'cache_on' => FALSE,
-// 	'cachedir' => '',
-// 	'char_set' => 'utf8',
-// 	'dbcollat' => 'utf8_general_ci',
-// 	'swap_pre' => '',
-// 	'encrypt' => FALSE,
-// 	'compress' => FALSE,
-// 	'stricton' => FALSE,
-// 	'failover' => array(),
-// 	'save_queries' => TRUE
-// );
 
 $db['default'] = array(
 	'dsn'	   => '',
